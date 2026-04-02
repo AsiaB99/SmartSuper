fa56b8804011dc58d86afb259f7b98a1fefab0c6
Asia <asiabosch@hotmail.es>
2026-04-03 00:49:57 +0200

    feat: registrar rutas resource de listas y redirección inicial

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ListaController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('listas.index');
 });
+
+Route::resource('listas', ListaController::class)->except(['show']);
